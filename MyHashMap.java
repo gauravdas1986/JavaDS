dbdb5d3c49c6fd03170089b87a4f68287c54b6b1
gauravdas1986 <32699166+gauravdas1986@users.noreply.github.com>
2018-04-24 12:08:10 +0530

    Update MyHashMap.java

--- MyHashMap.java
+++ MyHashMap.java
@@ -1,19 +1,10 @@
 package com.ds.hm;
 
 public class MyHashMap {
-	// for simplicity size is taken as 2^4
+	
 	private static final int SIZE = 16;
 	private Entry table[] = new Entry[SIZE];
 
-	/**
-	 * User defined map data structure with key and value.
-	 * 
-	 * This is also used as linked list in case multiple key-value pairs lead to
-	 * the same bucket with same hashcodes and different keys (collisions) using
-	 * the pointer 'next'.
-	 *
-	 * @author ntallapa
-	 */
 	class Entry {
 		Employee key;
 		String value;
@@ -37,44 +28,20 @@ public class MyHashMap {
 		}
 	}
 
-	/**
-	 * * REFERENCE: JAVA SOURCE CODE *
-	 * 
-	 * Applies a supplemental hash function to a given hashCode, which defends
-	 * against poor quality hash functions. This is critical because HashMap
-	 * uses power-of-two length hash tables, that otherwise encounter collisions
-	 * for hashCodes that do not differ in lower bits. Note: Null keys always
-	 * map to hash 0, thus index 0.
-	 */
 	private int getSupplementalHash(int h) {
-		// This function ensures that hashCodes that differ only by
-		// constant multiples at each bit position have a bounded
-		// number of collisions (approximately 8 at default load factor).
 		h ^= (h >>> 20) ^ (h >>> 12);
 		return h ^ (h >>> 7) ^ (h >>> 4);
 	}
 
-	/**
-	 * It makes sure the bucket number falls within the size of the hashmap
-	 * 
-	 * @param hash
-	 * @return returns index for hashcode hash
-	 */
 	private int getBucketNumber(int hash) {
 		return hash & (SIZE - 1);
 	}
 
-	/**
-	 * Associates the specified value with the specified key in this map. If the
-	 * map previously contained a mapping for the key, the old value is
-	 * replaced.
-	 */
 	public void put(Employee key, String value) {
-		// get the hashcode and regenerate it to be optimum
 		int userHash = key.hashCode();
 		int hash = getSupplementalHash(userHash);
 
-		// compute the bucket number (0-15 based on our default size)
+		// compute the bucket number
 		// this always results in a number between 0-15
 		int bucket = getBucketNumber(hash);
 		Entry existingElement = table[bucket];
@@ -102,16 +69,10 @@ public class MyHashMap {
 		table[bucket] = entryInOldBucket;
 	}
 
-	/**
-	 * Returns the entry associated with the specified key in the HashMap.
-	 * Returns null if the HashMap contains no mapping for the key.
-	 */
 	public Entry get(Employee key) {
-		// get the hashcode and regenerate it to be optimum
+		
 		int hash = getSupplementalHash(key.hashCode());
 
-		// compute the bucket number (0-15 based on our default size)
-		// this always results in a number between 0-15
 		int bucket = getBucketNumber(hash);
 
 		// get the element at the above bucket if it exists
@@ -146,7 +107,6 @@ public class MyHashMap {
 				.println("============== Adding Duplicate ===================");
 		Employee e1_dup = new Employee(100, "Niranjan");
 		tmhm.put(e1_dup, "dept12");
-		// the above value "dept12" should replace the old value "dept1"
 		Entry e = tmhm.get(e1_dup);
 		System.out.println("GET element - " + e.getKey() + "::" + e.getValue());
 
